74f5f0d8fffa197d4e40387e2e6796e6ec464a7d
Muriel Delvaux Daher <murieldaher@Muriels-MacBook-Pro.local>
2026-06-18 22:31:57 -0300

    feat: create index type to export api types

---
+++ src/types/api/index.ts
@@ -0,0 +1,5 @@
+export type { PatientCreatePayload, ClinicalDataPayload, PatientResponse,
+  PatientListItem, PatientDetailResponse, PatientLastPrediction,
+  HealthResponse } from './patient.api.types';
+
+export type { PredictionResponse, PredictPayload, FeatureImportanceResponse } from './prediction.api.types';
